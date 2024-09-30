2582d1345d681c44fb84fa4b08be8b47153463fd
Roman <rom4rsh@gmail.com>
2024-09-30 16:17:18 +0300

    hw3

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 1;
+        byte b = 2;
+        short c = 4;
+
+
+
+    }
+}
